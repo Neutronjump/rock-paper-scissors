5f508478cf766b70327fe7567aeb5f153ca90387
neutronjump <theamazingneutronjump@gmail.com>
2026-08-21 17:22:46 -0400

    Rewrite functions to use DOM + integrate UI Most functions were rewritten to work with the new UI. `console.log`s were replaced with `.textContent`s. The game now runs forever, and resets upon win/loss.

--- script.js
+++ script.js
@@ -1,92 +1,111 @@
-
 playGame()
-
 
 // Randomly chooses rock, paper or scissors string
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random()*3);
     let computerChoice;
-    if (randomNum === 0) computerChoice = "rock";
-    else if (randomNum === 1) computerChoice = "paper";
-    else computerChoice = "scissors";
+    if (randomNum === 0) computerChoice = 'rock';
+    else if (randomNum === 1) computerChoice = 'paper';
+    else computerChoice = 'scissors';
     return computerChoice;
 }
 
-// Assigns humanChoice to user input
-function getHumanChoice() {
-    let humanChoice = prompt("Please enter either rock, paper, or scissors:");
-    return humanChoice;
-}
-
-// Plays 5 rounds
+// Starts the game, then resets it on win/loss 
 function playGame() {
-    let humanScore = 0;
+    const gameMessage = document.querySelector(".result-message");
+    const roundScore = document.querySelector(".round-score");
+    const buttonDiv = document.querySelector('.button-container');
     let computerScore = 0;
-    let humanSelection
-    let computerSelection
+    let humanScore = 0;
 
-    for (i = 1; i <= 5; i++) {
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
+    displayStartInfo();
+
+    buttonDiv.addEventListener("click", getHumanChoice);
+
+// Shows starting info
+    function displayStartInfo() {
+        gameMessage.textContent = "Pick either rock, paper, or scissors, and I'll do the same.  Let's do this.";
+        roundScore.textContent = "Your score: " + humanScore + "\nMy score: " + computerScore;
     }
-    determineWinner();
+
+// Gets the user's choice and starts the round
+    function getHumanChoice(e) {
+        if (e.target.tagName === "BUTTON") {
+            const humanChoice = e.target.textContent;
+
+            playRound(humanChoice, getComputerChoice())
+        }
+
+    }  
 
 // Determines a winner based on the highest score
-    function determineWinner() {
-        if (humanScore > computerScore) {
-            console.log("Congrats! You beat me.");
-            console.log("If you were curious, you had " + humanScore + " point(s), and I had " + computerScore + " point(s).");
-        }
-        else if (humanScore < computerScore) {
-            console.log("Ha! I beat you!");
-            console.log("If you were curious, you had " + humanScore + " point(s)and I had " + computerScore + " point(s).");          
-        }
-        else {
-            console.log("It seems we have tied!")
-        }
+    function checkForWinner() {
+        if (humanScore === 5 || computerScore === 5) {
+            if (humanScore > computerScore) {
+                gameMessage.textContent = 'Congrats! You beat me.';
+                computerScore === 1? roundScore.textContent = 'You had ' + humanScore + ' points, and I had ' + computerScore + ' point.'
+                :
+                roundScore.textContent = 'You had ' + humanScore + ' points, and I had ' + computerScore + ' points.';
+                resetGame();
+            }
+            else if (humanScore < computerScore) {
+                gameMessage.textContent = 'Ha! I beat you!';
+                humanScore === 1? roundScore.textContent = 'You had ' + humanScore + ' point, and I had ' + computerScore + ' points.'
+                :
+                roundScore.textContent = 'You had ' + humanScore + ' points and I had ' + computerScore + ' points.';
+                resetGame();          
+            }
+    }
+}
 
+// Resets the scores
+    function resetGame() {
+        humanScore = 0;
+        computerScore = 0;
+        gameMessage.textContent = "Let's play again!";
     }
 
-// Plays a round and gives points and a little message based on whoever won
+// Plays a round, gives a point to the winner, and displays a message
     function playRound(humanChoice, computerChoice) {
         humanChoice = humanChoice.toLowerCase();
-        if (humanChoice === "rock" && computerChoice === "rock") {
-            console.log("Rock and rock is a tie!");
+        if (humanChoice === 'rock' && computerChoice === 'rock') {
+            gameMessage.textContent = 'Rock and rock is a tie!';
         }
-        else if (humanChoice === "paper" && computerChoice === "paper") {
-            console.log("Paper and paper is a tie!");
+        else if (humanChoice === 'paper' && computerChoice === 'paper') {
+            gameMessage.textContent = 'Paper and paper is a tie!';
         }
-        else if (humanChoice === "scissors" && computerChoice === "scissors") {
-            console.log("Scissors and scissors is a tie!");
+        else if (humanChoice === 'scissors' && computerChoice === 'scissors') {
+            gameMessage.textContent = 'Scissors and scissors is a tie!';
         }
-        else if (humanChoice === "rock" && computerChoice === "paper") {
-            console.log("Point for me! Paper covers rock.");
+        else if (humanChoice === 'rock' && computerChoice === 'paper') {
+            gameMessage.textContent = 'Point for me! Paper covers rock.';
             computerScore += 1;
         }
-        else if (humanChoice === "rock" && computerChoice === "scissors") {
-            console.log("Oh man, point for you. Rock smashes scissors!");
+        else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+            gameMessage.textContent = 'Oh man, point for you. Rock smashes scissors!';
             humanScore += 1;
         }
-        else if (humanChoice === "paper" && computerChoice === "rock") {
-            console.log("Point for you! Paper covers rock.");
+        else if (humanChoice === 'paper' && computerChoice === 'rock') {
+            gameMessage.textContent = 'Point for you! Paper covers rock.';
             humanScore += 1;
         }
-        else if (humanChoice === "paper" && computerChoice === "scissors") {
-            console.log("My point, scissors cut paper!");
+        else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+            gameMessage.textContent = 'My point, scissors cut paper!';
             computerScore += 1;
         }
-        else if (humanChoice === "scissors" && computerChoice === "rock") {
-            console.log("That's a point for me! Rock smashes scissors.");
+        else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+            gameMessage.textContent = 'That\'s a point for me! Rock smashes scissors.';
             computerScore += 1;
         }
-        else if (humanChoice === "scissors" && computerChoice === "paper") {
-            console.log("Thats a point for you! Scissors cut paper.");
+        else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+            gameMessage.textContent = 'That\'s a point for you! Scissors cut paper.';
             humanScore += 1;
         }
         else {
-            console.log("I think something went wrong, let's just try again.");
+            gameMessage.textContent = 'I think something went wrong, let\'s just try again.';
         }
+        roundScore.textContent = "Your score: " + humanScore + "\nMy score: " + computerScore;
+        checkForWinner();
         return;
     }
 }
